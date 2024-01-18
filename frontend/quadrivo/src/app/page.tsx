d062f00ae0c5e0b4bf8ad5655b25ffc457f0d100
Vedant_Tarale <vedanttarale.211ec257@nitk.edu.in>
2024-01-19 00:36:08 +0530

    setup components

--- frontend/quadrivo/src/app/page.tsx
+++ frontend/quadrivo/src/app/page.tsx
@@ -1,10 +1,17 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import CodeCard from "@/components/ui/code_card";
+import Footer from "@/components/ui/footer";
+import Hero from "@/components/ui/hero";
+import Navbar from "@/components/ui/navbar";
+import Tech from "@/components/ui/tech";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Button</Button>
+      <Navbar />
+      <Hero />
+      <Tech />
+      <CodeCard />
+      <Footer />
     </main>
   );
 }
