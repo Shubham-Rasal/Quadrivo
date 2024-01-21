fe3c3565d7eafcd5bf4f4bd1d658ccd655e1896a
Shubham-Rasal <bluequbits@gmail.com>
2024-01-21 18:12:30 +0530

    Update frontend configuration and components

--- frontend/src/app/agreements/page.tsx
+++ frontend/src/app/agreements/page.tsx
@@ -5,8 +5,8 @@ import React from "react";
 const AgreementPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <CreateAgreement />   
-      <Agreements/>
+      <CreateAgreement />
+      <Agreements />
     </div>
   );
 };
--- frontend/src/components/agreements.tsx
+++ frontend/src/components/agreements.tsx
@@ -1,7 +1,24 @@
 "use client";
 import { useContractRead } from "wagmi";
 import React from "react";
-
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { Separator } from "@/components/ui/separator";
 const Agreements = () => {
   const abi = [
     {
@@ -393,6 +410,28 @@ const Agreements = () => {
     functionName: "getDelegationAgreements",
   });
 
+  // name: string;
+  //   delegator: `0x${string}`;
+  //   amount: bigint;
+  //   duration: bigint;
+  //   start: bigint;
+  //   active: boolean;
+  //   tokenToOwn: `0x${string}`;
+  //   nftToOwn: `0x${string}`;
+  //   data: string;
+
+  type DelegationAgreement = {
+    name: string;
+    delegator: string;
+    amount: bigint;
+    duration: bigint;
+    start: bigint;
+    active: boolean;
+    tokenToOwn: string;
+    nftToOwn: string;
+    data: string;
+  };
+
   console.log(data);
 
   if (isLoading) return <div>Loading...</div>;
@@ -400,20 +439,56 @@ const Agreements = () => {
   if (!data) return <div>No data</div>;
 
   return (
-    <div>
-      {data.map((agreement: any, index) => {
+    <div className="grid grid-cols-2 gap-4 mt-2">
+      {data.map((agreement: DelegationAgreement, index) => {
         return (
-          <div key={index}>
-            <div>Name: {agreement.name}</div>
-            <div>Delegator: {agreement.delegator}</div>
-            <div>Amount: {agreement.amount}</div>
-            <div>Duration: {agreement.duration}</div>
-            <div>Start: {agreement.start}</div>
-            <div>Active: {agreement.active}</div>
-            <div>Token to own: {agreement.tokenToOwn}</div>
-            <div>NFT to own: {agreement.nftToOwn}</div>
-            <div>Data: {agreement.data}</div>
-          </div>
+          <Card key={index}>
+            <CardHeader className="grid grid-cols-[1fr_110px] items-start gap-4 space-y-0">
+              <div className="space-y-1">
+                <CardTitle>{agreement.name}</CardTitle>
+                <CardDescription>
+                  <span className="text-muted-foreground mr-2 text-slate-700">
+                    Delegator
+                  </span>
+                  {agreement.delegator}
+                </CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <div className="space-y-2">
+                <div className="space-y-1">
+                  <div className="flex items-center justify-between">
+                    <span className="text-muted-foreground">Amount</span>
+                    <span>{agreement.amount.toString()}</span>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <span className="text-muted-foreground">Duration</span>
+                    <span>{agreement.duration.toString()}</span>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <span className="text-muted-foreground">Start</span>
+                    <span>{agreement.start.toString()}</span>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <span className="text-muted-foreground">Active</span>
+                    <span>{agreement.active? "True" : "False"}</span>
+                  </div>
+                  <div className="flex items-center justify-between w-full">
+                    <span className="text-muted-foreground">TokenToOwn</span>
+                    <span>{agreement.tokenToOwn}</span>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <span className="text-muted-foreground">NftToOwn</span>
+                    <span>{agreement.nftToOwn}</span>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <span className="text-muted-foreground">Data</span>
+                    <span>{agreement.data}</span>
+                  </div>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
         );
       })}
     </div>
