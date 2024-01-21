3a446ac04e3eb217d85c4fce0f079371aff17925
Vedant_Tarale <vedanttarale.211ec257@nitk.edu.in>
2024-01-21 20:15:00 +0530

    add content

--- frontend/src/constants/data.tsx
+++ frontend/src/constants/data.tsx
@@ -2,34 +2,34 @@ export const aboutData = [
     {
         id: 1,
         title: "Deposit Matching Pool",
-        description: "",
-        content1: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam voluptas explicabo, velit laborum sunt, consequuntur distinctio a odit numquam nostrum, fuga suscipit impedit iusto? Architecto illo consectetur quo unde libero!",
-        content2: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam voluptas explicabo, velit laborum sunt, consequuntur distinctio a odit numquam nostrum, fuga suscipit impedit iusto? Architecto illo consectetur quo unde libero!",
-        src: "/graph.png"
+        description: "Boosting Impact: Your Deposits, Our Matching Magic",
+        content1: "Sponsors contribute ERC20 tokens (e.g., ETH, LINK) to a designated vault or smart contract.",
+        content2: "New shares or vault tokens are minted by the smart contract based on the contributed tokens, and these newly minted tokens are then allocated to the sponsors, providing them with a stake in the pool proportional to their contribution.",
+        src: "/deposit.jpg"
     },
     {
         id: 2,
         title: "Register Delegation Agreement",
-        description: "",
-        content1: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam voluptas explicabo, velit laborum sunt, consequuntur distinctio a odit numquam nostrum, fuga suscipit impedit iusto? Architecto illo consectetur quo unde libero!",
-        content2: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam voluptas explicabo, velit laborum sunt, consequuntur distinctio a odit numquam nostrum, fuga suscipit impedit iusto? Architecto illo consectetur quo unde libero!",
-        src: "/graph.png"
+        description: "Sponsor Goals, Project Commitments: Delegation Agreement Registered.",
+        content1: "Sponsors set rules for how funds should be used. This could include requirements like owning specific NFTs, following certain technical standards for network deployment, or maintaining a minimum level of token holdings.",
+        content2: "The criteria are designed to ensure that funded projects match the sponsors' overall objectives. By specifying criteria like NFT ownership or token holdings, sponsors aim to support initiatives that not only meet technical standards but also contribute to their strategic goals. This alignment increases the chances of successful collaboration and protects sponsors' investments.",
+        src: "/delegation.jpg"
     },
     {
         id: 3,
         title: "Register Project",
-        description: "",
-        content1: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam voluptas explicabo, velit laborum sunt, consequuntur distinctio a odit numquam nostrum, fuga suscipit impedit iusto? Architecto illo consectetur quo unde libero!",
-        content2: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam voluptas explicabo, velit laborum sunt, consequuntur distinctio a odit numquam nostrum, fuga suscipit impedit iusto? Architecto illo consectetur quo unde libero!",
-        src: "/graph.png"
+        description: "Project Innovation Unleashed: Registration Complete.",
+        content1: "When creating a project, agreements are checked across all chains to ensure consistency and compliance. This involves confirming that the project meets requirements and standards on each blockchain it interacts with.",
+        content2: "A total matching pool for the project is determined. This involves calculating the combined resources or support available from different sources, aligning with the project's needs and agreements.",
+        src: "/register.jpg"
     },
     {
         id: 4,
         title: "Fund Project",
-        description: "",
-        content1: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam voluptas explicabo, velit laborum sunt, consequuntur distinctio a odit numquam nostrum, fuga suscipit impedit iusto? Architecto illo consectetur quo unde libero!",
-        content2: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam voluptas explicabo, velit laborum sunt, consequuntur distinctio a odit numquam nostrum, fuga suscipit impedit iusto? Architecto illo consectetur quo unde libero!",
-        src: "/graph.png"
+        description: "Empower Projects: Token Contributions Ignite Impact.",
+        content1: "Users contribute tokens to support projects. This involves individuals providing their tokens as a form of funding for specific initiatives.",
+        content2: "Contributions from users are matched with quadratic GHO amounts from the vault. This means that the matching mechanism follows a quadratic formula, and funds from the vault are utilized to enhance the impact of user contributions, creating a more substantial effect on the supported projects.",
+        src: "/fund.jpg"
     }
 ]
 export default aboutData
